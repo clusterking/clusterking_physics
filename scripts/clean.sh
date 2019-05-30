fae5f79b5bf13f161d22c6a7f172c5ad6f553e48
Kilian Lieret <kilian.adriano.lieret@desy.de>
2019-05-30 13:01:54 +0200

    [Dev] Fix: remove egg-info folder

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,5 +5,5 @@ set -e
 thisDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sourceDir="${thisDir}/.."
 
-rm -f "${sourceDir}/*.egg-info"
+rm -rf "${sourceDir}/*.egg-info"
 rm -rf "${sourceDir}/build/**"
